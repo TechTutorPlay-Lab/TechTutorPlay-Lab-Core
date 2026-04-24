e44539fd9d537b168fcde4d758042331e30377ab
Daniele Vendramin <davendramin2012@gmail.com>
2026-04-24 16:08:02 +0200

    Add files via upload

---
+++ hello_lab.java
@@ -0,0 +1,6 @@
+public class hello_lab {
+
+    public static void main(String[] args) {
+        System.out.println("Benvenuto/a nella repository GitHub principale del TechTutorPlay Lab!");
+    }
+}
